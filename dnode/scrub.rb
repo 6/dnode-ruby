ddebff8f8fc64e0f8a49baa9c2b5145c8f20251b
James Halliday <substack@gmail.com>
2010-10-04 20:39:28 -0800

    untested scrubbing

--- dnode/scrub.rb
+++ dnode/scrub.rb
@@ -1,14 +1,29 @@
+require 'dnode/walk'
+
 class Scrub
     def initialize
         @callbacks = {}
+        @last_id = 0
     end
     
     def scrub args
-        { :object => args, :callbacks => {} }
+        callbacks = {}
+        walked = Walk.new(args).walk do |node|
+            if node.value.is_a? Proc then
+                id = @last_id
+                @last_id += 1
+                @callbacks[id] = node.value
+                callbacks[id] = node.path
+                node.value = '[Function]'
+            end
+        end
+        { :object => walked, :callbacks => callbacks }
     end
     
-    def unscrub req
-        # yield
-        req['arguments']
+    def unscrub req, &block
+        Walk.new(req).walk do |node|
+            id = @callbacks.detect{ |_,p| p == node.path }.first
+            node.value = block.call(id) unless id.nil?
+        end
     end
 end
